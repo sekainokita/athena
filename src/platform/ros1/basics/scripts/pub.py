fb2a322f10f30bb25f1ad6fa48d546bb96a4f7cc
Byoungman An <bman@keti.re.kr>
2023-03-10 15:18:13 +0900

    add platform

---
+++ src/platform/ros1/basics/scripts/pub.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Int32
+
+from math import * # * means All
+
+#1. Node - Init Setting
+rospy.init_node('pub_node')
+
+#2. Node - Publisher or Subscriber
+pub = rospy.Publisher('counter', Int32, queue_size=1)
+
+#3. Rate - Setting
+rate = rospy.Rate(10) # Send a data 10 times in 1s
+
+counter = 0
+while not rospy.is_shutdown():
+    #4. Node - publish
+    pub.publish(counter)
+    counter += 1
+
+    #5. Rating
+    rate.sleep() # Not sleep as C/C++, repeat as the rate value
