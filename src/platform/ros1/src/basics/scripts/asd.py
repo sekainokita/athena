27b86a5243dc330d2da5c5d2acc2dccfe7a999a2
Byoungman An <bman@keti.re.kr>
2023-03-10 17:57:54 +0900

    update the algorithm

--- src/platform/ros1/src/basics/scripts/asd.py
+++ src/platform/ros1/src/basics/scripts/asd.py
@@ -1,16 +1,24 @@
+from time import time
 
 snail = 0
 sun = 5
 night = 3
-distance = 100
+distance = 1000000000000
 day = 0
 
-while True:
-        day += 1
-        snail += 5
-        if snail >= 100:
-                break
-        else:
-                snail -= 3
+start_time = time()
 
+# while True:
+#         day += 1
+#         snail += 5
+#         if snail >= distance:
+#                 break
+#         else:
+#                 snail -= 3
+
+day = ((distance - night) // (sun - night)) + 1
+
+end_time = time()
+
+print("time:", end_time - start_time)
 print("day:", day)
