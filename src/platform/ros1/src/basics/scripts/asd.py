5a20a647c597e070a9dcbe109b41b23e50cc5eff
Byoungman An <bman@keti.re.kr>
2023-03-10 17:44:47 +0900

    update asd.py

--- src/platform/ros1/src/basics/scripts/asd.py
+++ src/platform/ros1/src/basics/scripts/asd.py
@@ -6,11 +6,11 @@ distance = 100
 day = 0
 
 while True:
-        day = +1
+        day += 1
         snail += 5
         if snail >= 100:
                 break
         else:
-                snail -=3
+                snail -= 3
 
 print("day:", day)
