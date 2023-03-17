624cf4938571c82cc0aab79fac35e5998120a0e3
Byoungman An <bman@keti.re.kr>
2023-03-17 16:03:28 +0900

    gstreamer

--- src/packages/scripts/install_packages_20.04.sh
+++ src/packages/scripts/install_packages_20.04.sh
@@ -26,6 +26,9 @@ apt-get install -y net-tools
 apt-get install -y minicom
 apt-get install -y libjpeg-dev libtiff5-dev libpng-dev
 apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev libx264-dev libxine2-dev
+apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base
+apt-get install -y gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc
+apt-get install -y gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
 pip3 install onnx-graphsurgeon
 pip3 install opencv-contrib-python
