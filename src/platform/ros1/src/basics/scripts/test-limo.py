5b29659566aa0c37a2836c96047c23d2eb162118
Byoungman An <bman@keti.re.kr>
2023-03-24 15:36:40 +0900

    update limo examples

--- src/platform/ros1/src/basics/scripts/test-limo.py
+++ src/platform/ros1/src/basics/scripts/test-limo.py
@@ -3,12 +3,24 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from math import *
 
 rospy.init_node('laser_scan_node')
 laser_data = LaserScan()
 
 def laser_CB(msg):
-    print(msg)
+    print(f"angle_min : {msg.angle_min*180/pi}")
+    print(f"angle_max : {msg.angle_max*180/pi}")
+    print(f"angle_increment : {msg.angle_increment*180/pi}")
+    print(f"len_ranges : {len(msg.ranges)}")
+
+    degrees = [
+                (msg.angle_min+(i*msg.angle_increment))*180/pi
+                for i, data in enumerate(msg.ranges)
+            ]
+    
+    print(f'degrees:{degrees}')
+
 
 rospy.Subscriber("/scan", LaserScan, callback=laser_CB)
 
