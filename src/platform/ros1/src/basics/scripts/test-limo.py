bf2f18fb0eb57830d7c398c42cb8400b41d54b0b
Byoungman An <bman@keti.re.kr>
2023-03-24 15:50:21 +0900

    update lidar example

--- src/platform/ros1/src/basics/scripts/test-limo.py
+++ src/platform/ros1/src/basics/scripts/test-limo.py
@@ -5,23 +5,32 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from math import *
 
-rospy.init_node('laser_scan_node')
-laser_data = LaserScan()
+class Limo_lidar:
+    def __init__(self):
+        rospy.init_node('laser_scan_node')
+        rospy.Subscriber("/scan", LaserScan, callback=self.laser_CB)
+        self.lidar_flag = False
 
-def laser_CB(msg):
-    print(f"angle_min : {msg.angle_min*180/pi}")
-    print(f"angle_max : {msg.angle_max*180/pi}")
-    print(f"angle_increment : {msg.angle_increment*180/pi}")
-    print(f"len_ranges : {len(msg.ranges)}")
+    def laser_CB(self, msg):
+        # print(f"angle_min : {msg.angle_min*180/pi}")
+        # print(f"angle_max : {msg.angle_max*180/pi}")
+        # print(f"angle_increment : {msg.angle_increment*180/pi}")
+        # print(f"len_ranges : {len(msg.ranges)}")
+        
+        if self.lidar_flag == False:
+            self.degrees = [(msg.angle_min+(i*msg.angle_increment))*180/pi for i, data in enumerate(msg.ranges)]
+            self.lidar_flag = True
+        
+        for i, data in enumerate(msg.ranges):
+            if -30 < self.degrees[i] < 30:
+                print(f"{self.degrees[i]} : {msg.ranges[i]}")
+                pass
 
-    degrees = [
-                (msg.angle_min+(i*msg.angle_increment))*180/pi
-                for i, data in enumerate(msg.ranges)
-            ]
-    
-    print(f'degrees:{degrees}')
+#        print(f'degrees:{degrees}')
 
-
-rospy.Subscriber("/scan", LaserScan, callback=laser_CB)
-
-rospy.spin()
+if __name__ == "__main__":
+    limo_lidar = Limo_lidar()
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
