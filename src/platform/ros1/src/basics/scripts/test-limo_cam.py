e202b46245f4b809b48bea6c1fc5f00eef66ae72
Byoungman An <bman@keti.re.kr>
2023-03-24 17:07:11 +0900

    apply mask functions

--- src/platform/ros1/src/basics/scripts/test-limo_cam.py
+++ src/platform/ros1/src/basics/scripts/test-limo_cam.py
@@ -6,6 +6,8 @@ from sensor_msgs.msg import CompressedImage
 from math import *
 import cv2
 from cv_bridge import CvBridge
+import numpy as np
+
 
 class Limo_camera:
     def __init__(self):
@@ -14,9 +16,25 @@ class Limo_camera:
         
         self.cvbridge = CvBridge()
 
+   # def img_binary(self, blend_line):
+        #bin = 
+
     def camera_CB(self, msg):
         img = self.cvbridge.compressed_imgmsg_to_cv2(msg)
+        img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+        lower_yellow = np.array([0,50,0])
+        upper_yellow = np.array([45,255,255])
+        yellow_mask = cv2.inRange(img_hsv, lower_yellow, upper_yellow)
+
+        yellow_binary = np.zeros_like(yellow_mask)
+        yellow_binary[yellow_mask != 0] = 1
+        print(len(yellow_mask.nonzero()[0]))
+
+        img_out = cv2.bitwise_and(img, img, mask=yellow_mask)
+
         cv2.imshow("img", img)
+        cv2.imshow("img_out", img_out)        
+        cv2.imshow("yellow_mask", yellow_mask)
         cv2.waitKey(1)
 
 if __name__ == "__main__":
