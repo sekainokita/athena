df322047ab61c7a6928463555e4649c870e2a26e
Byoungman An <bman@keti.re.kr>
2023-03-27 14:05:39 +0900

    add packages

--- src/packages/scripts/install_packages_20.04.sh
+++ src/packages/scripts/install_packages_20.04.sh
@@ -61,6 +61,7 @@ apt-get install -y gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1
 apt-get install -y gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 apt-get install -y libpcap-dev
 
+pip3 install nvidia-pyindex
 pip3 install onnx-graphsurgeon
 pip3 install opencv-contrib-python
 
