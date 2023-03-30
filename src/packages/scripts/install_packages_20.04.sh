795d59553d6376aefa59307ae1ca0d61544fd28b
Byoungman An <bman@keti.re.kr>
2023-03-30 16:55:53 +0900

    add gst packages

--- src/packages/scripts/install_packages_20.04.sh
+++ src/packages/scripts/install_packages_20.04.sh
@@ -59,6 +59,7 @@ apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev
 apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base
 apt-get install -y gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc
 apt-get install -y gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+apt-get install -y libgstrtspserver-1.0-dev gstreamer1.0-rtsp
 apt-get install -y libpcap-dev
 
 pip3 install nvidia-pyindex
