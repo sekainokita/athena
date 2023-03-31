ee3acb391e512576cdf4386a3d40c459794c1f41
Byoungman An <bman@keti.re.kr>
2023-03-31 10:23:55 +0900

    update comments

--- src/packages/scripts/install_packages_20.04.sh
+++ src/packages/scripts/install_packages_20.04.sh
@@ -75,7 +75,7 @@ apt install ros-noetic-desktop-full
 apt search ros-noetic
 
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-echo "change own ROS path" >> ~/.bashrc
+echo "echo change own ROS path" >> ~/.bashrc
 echo "export ROS_IP=$(hostname -I | awk '{ print $1 }')" >> ~/.bashrc
 echo "export ROS_MASTER_URI=http://localhost:11311" >> ~/.bashrc
 echo "export ROS_HOSTNAME=$ROS_IP" >> ~/.bashrc
