85d6769239dc37c72faf844e0a63af2250595e0e
Byoungman An <bman@keti.re.kr>
2023-03-31 10:07:21 +0900

    add ROS env

--- src/packages/scripts/install_packages_20.04.sh
+++ src/packages/scripts/install_packages_20.04.sh
@@ -66,6 +66,22 @@ pip3 install nvidia-pyindex
 pip3 install onnx-graphsurgeon
 pip3 install opencv-contrib-python
 
+# ROS
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-get install -y curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+apt-get update
+apt install ros-noetic-desktop-full
+apt search ros-noetic
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+apt-get install -y python3-rosdep
+rosdep init
+rosdep update
+apt-get install -y ros-noetic-ros-tutorials
+
 # add folders
 mkdir /tftpboot
 chown nobody:nogroup /tftpboot
