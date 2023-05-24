d1f4a891253dc164152406d5aa51161e559e61b4
Byoungman An <bman@keti.re.kr>
2023-05-24 20:18:35 +0900

    [KETI] set a payload size from 300 to 1024

--- src/framework/include/db_manager.h
+++ src/framework/include/db_manager.h
@@ -53,7 +53,7 @@
 #define CLI_DB_V2X_DEFAULT_DEVICE_ID              0x12345678
 #define CLI_DB_V2X_DEFAULT_TIMESTAMP              2023032314344766828
 #define CLI_DB_V2X_DEFAULT_HW_VER                 0x1234
-#define CLI_DB_V2X_DEFAULT_PAYLOAD_LEN            300
+#define CLI_DB_V2X_DEFAULT_PAYLOAD_LEN            1024
 
 /***************************** Enum and Structure ****************************/
 
