f5b082640baa39e8494f12b42449aa92b929c7ba
Byoungman An <bman@keti.re.kr>
2023-06-29 19:20:30 +0900

    [KETI][CLI] implement a prototype of CLI DI

--- src/apps/cli/cli.c
+++ src/apps/cli/cli.c
@@ -303,6 +303,12 @@ static int32_t P_CLI_InitCmd(void)
         PrintError("CLI_PLATOONING_InitCmds() is failed! [nRet:%d]", nRet);
     }
 
+    nRet = CLI_DI_InitCmds();
+    if (nRet != APP_OK)
+    {
+        PrintError("CLI_DI_InitCmds() is failed! [nRet:%d]", nRet);
+    }
+
     return nRet;
 }
 
---
+++ src/apps/cli/cli_di.c
@@ -0,0 +1,116 @@
+/******************************************************************************
+*
+* Copyright (C) 2023 - 2028 KETI, All rights reserved.
+*                           (Korea Electronics Technology Institute)
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running for Korean Government Project, or
+* (b) that interact with KETI project/platform.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* KETI BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the KETI shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from KETI.
+*
+******************************************************************************/
+/******************************************************************************/
+/**
+*
+* @file cli_di.c
+*
+* @note
+*
+* CLI DI Source
+*
+******************************************************************************/
+
+
+/***************************** Include ***************************************/
+#include "cli.h"
+#include "app.h"
+#include "di.h"
+
+/***************************** Definition ************************************/
+
+/***************************** Static Variable *******************************/
+
+
+/***************************** Function Protype ******************************/
+
+static int P_CLI_DI(CLI_CMDLINE_T *pstCmd, int argc, char *argv[])
+{
+    int32_t nRet = APP_OK;
+    char *pcCmd;
+
+    UNUSED(argc);
+
+    if(argv == NULL)
+    {
+        PrintError("argv == NULL!!");
+        return nRet;
+    }
+
+    pcCmd = CLI_CMD_GetArg(pstCmd, CMD_0);
+    if (pcCmd == NULL)
+    {
+        return CLI_CMD_Showusage(pstCmd);
+    }
+    else
+    {
+        pcCmd = CLI_CMD_GetArg(pstCmd, CMD_0);
+        if(IS_CMD(pcCmd, "test"))
+        {
+            for(int i = 0; i < CMD_MAX; i++)
+            {
+                pcCmd = CLI_CMD_GetArg(pstCmd, i);
+                PrintDebug("pcCmd[idx:%d][value:%s]", i, pcCmd);
+            }
+        }
+        else
+        {
+            return CLI_CMD_Showusage(pstCmd);
+        }
+    }
+
+	return nRet;
+}
+
+int32_t CLI_DI_InitCmds(void)
+{
+    int32_t nRet = APP_ERROR;
+
+    nRet = CLI_CMD_AddCmd("di",
+               P_CLI_DI,
+               NULL,
+               "help for DI commands",
+               "di [enter command]\n\n"
+               "Without any parameters, the 'di' show a description\n"
+               "of available commands. For more details on a command, type and enter 'di'\n"
+               "and the command name.\n\n"
+               "di test        test di command\n",
+               "");
+    if(nRet != APP_OK)
+    {
+        PrintError("CLI_CMD_AddCmd() is failed! [nRet:%d]", nRet);
+    }
+
+    return nRet;
+}
+
