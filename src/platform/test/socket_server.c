5f0bc8feb4e3e8e974d01c0e227de60c691e18a3
Byoungman An <bman@keti.re.kr>
2024-06-07 16:56:14 +0900

    KETI add socket_server

---
+++ src/platform/test/socket_server.c
@@ -0,0 +1,59 @@
+// socket_server.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        int valread = read(new_socket, buffer, 1024);
+        if (valread > 0) {
+            printf("%s", buffer);
+            memset(buffer, 0, 1024); // buffer를 초기화하여 반복 읽기 준비
+        } else {
+            break; // 연결이 종료되면 루프 종료
+        }
+    }
+
+    close(new_socket);
+    close(server_fd);
+    return 0;
+}
