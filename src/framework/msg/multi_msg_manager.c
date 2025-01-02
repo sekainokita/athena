6fcf6bd49481c9e0132529080492aed081233641
Leejimin98 <kng05157@keti.re.kr>
2025-01-02 13:55:41 +0900

    [KETI] remove single example

--- src/framework/msg/multi_msg_manager.c
+++ src/framework/msg/multi_msg_manager.c
@@ -133,25 +133,6 @@ static uint32_t s_unMultiV2xMsgTxLen = 0, s_unMultiV2xMsgRxLen = 0;
 /////////////////////////////////////////////////////////////////////////////////////////
 /* Global Variable Value */
 #if defined(CONFIG_MULTI_DEV)
-V2xAction_t e_action_g = eV2xAction_ADD;
-V2xPayloadType_t e_payload_type_g = eRaw;
-V2xPsid_t psid_g = 5271;
-V2XCommType_t e_comm_type_g = eV2XCommType_5GNRV2X;
-V2xPowerDbm_t tx_power_g = 20;
-V2xSignerId_t e_signer_id_g = eV2xSignerId_UNSECURED;
-V2xMsgPriority_t e_priority_g = eV2xPriority_CV2X_PPPP_0;
-uint32_t tx_cnt_g = 100;
-uint32_t tx_delay_g = 100;
-V2xFrequency_t freq_g = 5900;
-V2xDataRate_t e_data_rate_g = eV2xDataRate_6MBPS;
-V2xTimeSlot_t e_time_slot_g = eV2xTimeSlot_Continuous;
-uint8_t peer_mac_addr_g[MAC_EUI48_LEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-uint32_t transmitter_profile_id_g = 100;
-uint32_t peer_l2id_g = 0;
-
-#if defined(CONFIG_TEMP_OBU_TEST)
-uint32_t delay_time_sec_g = 10;
-#endif
 
 static void P_MULTI_MSG_NABAGER_PrintMsgInfo(int msqid)
 {
@@ -2518,192 +2499,6 @@ static void *P_MULTI_MSG_MANAGER_RxTask(void *arg)
     return NULL;
 }
 
-#if defined(CONFIG_TEMP_OBU_TEST)
-void *MULTI_MSG_MANAGER_TxTask(void *arg)
-{
-    (void)arg;
-
-    // Prepare the Ext_WSReq_t structure
-    int db_v2x_tmp_size = sizeof(DB_V2X_T) + MULTI_SAMPLE_V2X_MSG_LEN;
-    int v2x_tx_pdu_size = sizeof(Ext_V2X_TxPDU_t) + db_v2x_tmp_size;
-
-    Ext_V2X_TxPDU_t *v2x_tx_pdu_p = NULL;
-    DB_V2X_T *db_v2x_tmp_p = NULL;
-    MULTI_MSG_MANAGER_TX_EVENT_MSG_T stEventMultiMsg;
-
-    v2x_tx_pdu_p = malloc(v2x_tx_pdu_size);
-    if(v2x_tx_pdu_p == NULL)
-    {
-        PrintError("malloc() is failed! [NULL]");
-        return NULL;
-    }
-
-    memset(&stEventMultiMsg, 0, sizeof(MULTI_MSG_MANAGER_TX_T));
-    memset(v2x_tx_pdu_p, 0, sizeof(Ext_V2X_TxPDU_t));
-
-    v2x_tx_pdu_p->ver = htons(MULTI_SAMPLE_V2X_API_VER);
-    v2x_tx_pdu_p->e_payload_type = e_payload_type_g;
-    v2x_tx_pdu_p->psid = htonl(psid_g);
-    v2x_tx_pdu_p->tx_power = tx_power_g;
-    v2x_tx_pdu_p->e_signer_id = e_signer_id_g;
-    v2x_tx_pdu_p->e_priority = e_priority_g;
-
-    if (e_comm_type_g == eV2XCommType_LTEV2X || e_comm_type_g == eV2XCommType_5GNRV2X)
-    {
-        v2x_tx_pdu_p->magic_num = htons(MAGIC_CV2X_TX_PDU);
-        v2x_tx_pdu_p->u.config_cv2x.transmitter_profile_id = htonl(transmitter_profile_id_g);
-        v2x_tx_pdu_p->u.config_cv2x.peer_l2id = htonl(peer_l2id_g);
-    }
-    else if (e_comm_type_g == eV2XCommType_DSRC)
-    {
-        v2x_tx_pdu_p->magic_num = htons(MAGIC_DSRC_TX_PDU);
-        v2x_tx_pdu_p->u.config_wave.freq = htons(freq_g);
-        v2x_tx_pdu_p->u.config_wave.e_data_rate = htons(e_data_rate_g);
-        v2x_tx_pdu_p->u.config_wave.e_time_slot = e_time_slot_g;
-        memcpy(v2x_tx_pdu_p->u.config_wave.peer_mac_addr, peer_mac_addr_g, MAC_EUI48_LEN);
-    }
-
-    // Payload = KETI Format
-    v2x_tx_pdu_p->v2x_msg.length = htons(db_v2x_tmp_size);
-
-    db_v2x_tmp_p = malloc(db_v2x_tmp_size);
-    if(db_v2x_tmp_p == NULL)
-    {
-        PrintError("malloc() is failed! [NULL]");
-        return NULL;
-    }
-
-    memset(db_v2x_tmp_p, 0, db_v2x_tmp_size);
-
-    db_v2x_tmp_p->eDeviceType = DB_V2X_DEVICE_TYPE_OBU;
-    db_v2x_tmp_p->eTeleCommType = DB_V2X_TELECOMM_TYPE_5G_PC5;
-    db_v2x_tmp_p->unDeviceId = 0;
-    db_v2x_tmp_p->ulTimeStamp = 0ULL;
-    db_v2x_tmp_p->eServiceId = DB_V2X_SERVICE_ID_PLATOONING;
-    db_v2x_tmp_p->eActionType = DB_V2X_ACTION_TYPE_REQUEST;
-    db_v2x_tmp_p->eRegionId = DB_V2X_REGION_ID_SEOUL;
-    db_v2x_tmp_p->ePayloadType = DB_V2X_PAYLOAD_TYPE_SAE_J2735_BSM;
-    db_v2x_tmp_p->eCommId = DB_V2X_COMM_ID_V2V;
-    db_v2x_tmp_p->usDbVer = 0;
-    db_v2x_tmp_p->usHwVer = 0;
-    db_v2x_tmp_p->usSwVer = 0;
-    db_v2x_tmp_p->ulPayloadLength = MULTI_SAMPLE_V2X_MSG_LEN;
-    db_v2x_tmp_p->ulReserved = 0;
-
-    memcpy(v2x_tx_pdu_p->v2x_msg.data, db_v2x_tmp_p, db_v2x_tmp_size);
-
-    printf("\nV2X TX PDU>>\n"
-    "  magic_num        : 0x%04X\n"
-    "  ver              : 0x%04X\n"
-    "  e_payload_type   : %d\n"
-    "  psid             : %u\n"
-    "  tx_power         : %d\n"
-    "  e_signer_id      : %d\n"
-    "  e_priority       : %d\n",
-    ntohs(v2x_tx_pdu_p->magic_num),
-    ntohs(v2x_tx_pdu_p->ver),
-    v2x_tx_pdu_p->e_payload_type,
-    ntohl(v2x_tx_pdu_p->psid),
-    v2x_tx_pdu_p->tx_power,
-    v2x_tx_pdu_p->e_signer_id,
-    v2x_tx_pdu_p->e_priority);
-
-    if (e_comm_type_g == eV2XCommType_LTEV2X || e_comm_type_g == eV2XCommType_5GNRV2X)
-    {
-        printf("  u.config_cv2x.transmitter_profile_id : %u\n"
-        "  u.config_cv2x.peer_l2id              : %u\n",
-        ntohl(v2x_tx_pdu_p->u.config_cv2x.transmitter_profile_id),
-        ntohl(v2x_tx_pdu_p->u.config_cv2x.peer_l2id));
-    }
-    else if (e_comm_type_g == eV2XCommType_DSRC)
-    {
-        printf("  u.config_wave.freq                  : %d\n"
-        "  u.config_wave.e_data_rate           : %d\n"
-        "  u.config_wave.e_time_slot           : %d\n"
-        "  u.config_wave.peer_mac_addr         : %s\n",
-        ntohs(v2x_tx_pdu_p->u.config_wave.freq),
-        ntohs(v2x_tx_pdu_p->u.config_wave.e_data_rate),
-        v2x_tx_pdu_p->u.config_wave.e_time_slot,
-        v2x_tx_pdu_p->u.config_wave.peer_mac_addr);
-    }
-
-    uint32_t i;
-    ssize_t n;
-
-    for (i = 0; i < tx_cnt_g; i++)
-    {
-        n = send(s_nMultiSocketHandle, v2x_tx_pdu_p, v2x_tx_pdu_size, 0);
-        if (n < 0)
-        {
-            PrintError("send() is failed!!");
-            break;
-        }
-        else if (n != v2x_tx_pdu_size)
-        {
-            PrintError("send() sent a different number of bytes than expected!!");
-            break;
-        }
-        else
-        {
-            PrintDebug("tx send success (%ld bytes) : [%u/%u]", n, i + 1, tx_cnt_g);
-        }
-
-        P_MULTI_MSG_MANAGER_SendTxMsgToDbMgr(&stEventMultiMsg);
-
-        usleep((1000 * tx_delay_g));
-    }
-
-    free(v2x_tx_pdu_p);
-    free(db_v2x_tmp_p);
-
-    return NULL;
-}
-
-void *MULTI_MSG_MANAGER_RxTask(void *arg)
-{
-    (void)arg;
-
-    uint8_t buf[4096] = {0};
-    int nRecvLen = -1;
-    time_t start_time = time(NULL);
-
-    while (1)
-    {
-        time_t current_time = time(NULL);
-        if (current_time - start_time >= delay_time_sec_g)
-        {
-            break;
-        }
-
-        nRecvLen = recv(s_nMultiSocketHandle, buf, sizeof(buf), 0);
-        if (nRecvLen < 0)
-        {
-            if ((errno != EAGAIN) && (errno != EWOULDBLOCK)
-            {
-                PrintError("recv() is failed!!");
-                break;
-            }
-            else
-            {
-                usleep(10000);
-                continue;
-            }
-        }
-        else if (nRecvLen == 0)
-        {
-            PrintError("recv()'s connection is closed by peer!!");
-            break;
-        }
-        else
-        {
-            PrintDebug("recv() is success : nRecvLen[%u]", nRecvLen);
-        }
-    }
-
-	return NULL;
-}
-#endif
-
 int32_t P_MULTI_MSG_MANAGER_CreateTask(void)
 {
     int32_t nRet = FRAMEWORK_ERROR;
@@ -2759,28 +2554,6 @@ int32_t P_MULTI_MSG_MANAGER_CreateTask(void)
 #endif
     return nRet;
 }
-
-#if defined(CONFIG_TEMP_OBU_TEST)
-int32_t P_MULTI_MSG_MANAGER_CreateObuTask(void)
-{
-    int32_t nRet = FRAMEWORK_ERROR;
-    pthread_t h_TxTask;
-    pthread_t h_RxTask;
-
-    void *pTxTaskRet;
-    void *pRxTaskRet;
-
-    pthread_create(&h_TxTask, NULL, MULTI_MSG_MANAGER_TxTask, NULL);
-    pthread_create(&h_RxTask, NULL, MULTI_MSG_MANAGER_RxTask, NULL);
-
-    pthread_join(h_TxTask, &pTxTaskRet);
-    pthread_join(h_RxTask, &pRxTaskRet);
-
-    nRet = FRAMEWORK_OK;
-
-    return nRet;
-}
-#endif
 
 int32_t MULTI_MSG_MANAGER_Transmit(MULTI_MSG_MANAGER_TX_T *pstMultiMsgMgrTx, DB_V2X_T *pstDbV2x, void *pPayload)
 {
@@ -2936,15 +2709,6 @@ int32_t MULTI_MSG_MANAGER_Open(MULTI_MSG_MANAGER_T *pstMultiMsgManager)
     if (nRet != FRAMEWORK_OK)
     {
         PrintError("P_MULTI_MSG_MANAGER_SetV2xWsrSetting() is failed!!, nRet[%d]", nRet);
-        return nRet;
-    }
-#endif
-
-#if defined(CONFIG_TEMP_OBU_TEST)
-    nRet = P_MULTI_MSG_MANAGER_CreateObuTask();
-    if (nRet != FRAMEWORK_OK)
-    {
-        PrintError("P_MULTI_MSG_MANAGER_CreateObuTask() is failed!!, nRet[%d]", nRet);
         return nRet;
     }
 #endif
